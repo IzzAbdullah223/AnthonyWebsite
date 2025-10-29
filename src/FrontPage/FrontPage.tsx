0bac90c01f369e4d136aa51cf3c981b0fb6e1cda
Izz Abdallah <x3zoabdullah@gmail.com>
2025-10-30 02:19:04 +0400

    Image placeholder

--- src/FrontPage/FrontPage.tsx
+++ src/FrontPage/FrontPage.tsx
@@ -27,27 +27,7 @@ function FrontPage(){
             </div>
         </div>
         <div className={FrontPageCSS.BoxesContainer}>
-            <div className={FrontPageCSS.LeftBox}>
-                <h3>Client Testimonials</h3>
-                <p>
-                "Paul Habibi is a brilliant, diligent and credible expert witness, and perhaps the finest I have seen throughout my 20-plus
-                years as a litigator. Having a UCLA professor and acclaimed investor embodied in one expert makes him a great asset in
-                high stakes, bet-the-company litigation."
-                </p>
-                <div className={FrontPageCSS.Name}> 
-                    <div>Kathleen Smalley</div>
-                    <div>Former Partner, Boies Schiller Flexner LLP (Ret.)</div>
-                </div>
-                <div className={FrontPageCSS.DotsContainer}>
-                        <div className={`${FrontPageCSS.Dot} ${FrontPageCSS.ActiveDot}`}></div>
-                        <div className={FrontPageCSS.Dot}></div>
-                        <div className={FrontPageCSS.Dot}></div>
-                        <div className={FrontPageCSS.Dot}></div>
-                        <div className={FrontPageCSS.Dot}></div>
-                        <div className={FrontPageCSS.Dot}></div>
-                        <div className={FrontPageCSS.Dot}></div>
-                </div>
-            </div>
+         <div>IMAGE PLACEHOLDER</div>
         </div>
         <hr></hr>
     </div>)
